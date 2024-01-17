9d357d308a3684e0ee3833499f5ff5fa30431fe9
ekraouf <mahmoudabdelraouf.me@gmail.com>
2024-01-17 23:29:10 +0200

    feat: modify sp

--- resources/views/specialist/layouts/navbar.blade.php
+++ resources/views/specialist/layouts/navbar.blade.php
@@ -2,14 +2,19 @@
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href={{route('specialists.orders.new')}} class="nav-link">الطلبات</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="../../index3.html" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Contact</a>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <x-dropdown-link :href="route('logout')"
+                                 onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                    {{ __('تسجيل الخروج') }}
+                </x-dropdown-link>
+            </form>
         </li>
     </ul>
 
